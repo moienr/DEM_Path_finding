a9c8fc84f6a32de60f21b427590e13a89a27bf9d
moienr <104892937+moienr@users.noreply.github.com>
2022-09-20 04:13:35 +0430

    Add files via upload

---
+++ Utils/buffer.py
@@ -0,0 +1,51 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.ndimage.filters import generic_filter
+from scipy.stats import mode
+from scipy import ndimage, misc
+mainpath= plt.imread('mainpath.tif')
+
+mainpath = mainpath[:,:,1]
+
+m = np.zeros((320, 216))
+m[:,:-1]  = mainpath 
+print(m.shape)
+mainpath = m
+
+count = np.count_nonzero(mainpath)
+print(count)
+
+
+output_path = np.load('best.npy')
+print(output_path.shape)
+
+mainpaththick = ndimage.maximum_filter(mainpath, size=5)
+
+x = (mainpaththick+output_path)
+y =x>125
+
+count = np.count_nonzero(y)
+print(count)
+
+
+y= y*255
+
+#applying 3x3 max filter to make the lines thicker
+conjunction = ndimage.maximum_filter(y, size=2)
+
+
+
+fig, (ax1, ax2,ax3) = plt.subplots(1, 3)
+fig.suptitle('Real Path vs AI')
+ax1.imshow((mainpaththick>1)*125)
+ax2.imshow(output_path)
+ax3.imshow(conjunction)
+ax1.axis('off')
+ax2.axis('off')
+ax3.axis('off')
+ax1.set_title('5 pixel bufferd Real Path')
+ax2.set_title('AI Path')
+ax3.set_title('Overlaping pixels')
+plt.show()
+
+
